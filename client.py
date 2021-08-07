ce9aef0281b4c2e4255fcce7608fee36161163e7
Oliver-CoderAcademy <oliver.lavers@coderacademy.edu.au>
2021-08-07 14:03:03 +1000

    wrote the client module

---
+++ client.py
@@ -0,0 +1,18 @@
+import socket
+
+client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+# instance = Class(argument1, argument2)
+
+client_socket.connect(("127.0.0.1", 65432))
+
+message_to_send = input("What would you like to send to the server? \n")
+
+client_socket.sendall(bytes(message_to_send, "utf-8"))
+
+received_message = client_socket.recv(1024)
+
+print("You received from the server this message:")
+print(repr(received_message))
+# print a string representation of the bytes received
+
+client_socket.close()
